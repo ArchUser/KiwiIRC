38839e521362d577dcefd27d6bfea04fa2a4f5e1
Darren <darren@darrenwhitlen.com>
2012-11-08 19:26:11 +0000

    Client: Improved gateway event handlers

--- client/assets/dev/model_gateway.js
+++ client/assets/dev/model_gateway.js
@@ -47,7 +47,60 @@ _kiwi.model.Gateway = function () {
         
         // For ease of access. The socket.io object
         this.socket = this.get('socket');
+
+        this.applyEventHandlers();
     };
+
+
+
+    this.applyEventHandlers = function () {
+        var that = this;
+        
+        // Some easier handler events
+        this.on('onmsg', function (event) {
+            var source,
+                is_pm = (event.channel == that.get('nick'));
+
+            source = is_pm ? event.nick : event.channel;
+            
+            that.trigger('msg:' + source, event);
+
+            if (is_pm) {
+                that.trigger('pm', event);
+                that.trigger('pm:' + source, event);
+            }
+        }, this);
+
+
+        this.on('onnotice', function (event) {
+            // The notice towards a channel or a query window?
+            var source = event.target || event.nick;
+
+            this.trigger('notice', event);
+            this.trigger('notice:' + source, event);
+        }, this);
+
+
+        this.on('onaction', function (event) {
+            var source,
+                is_pm = (event.channel == that.get('nick'));
+
+            source = is_pm ? event.nick : event.channel;
+            
+            that.trigger('action:' + source, event);
+
+            if (is_pm) {
+                that.trigger('action', event);
+                that.trigger('action:' + source, event);
+            }
+        }, this);
+
+
+        this.on('ontopic', function (event) {
+            that.trigger('topic', event);
+            that.trigger('topic:' + event.channel, event);
+        });
+    },
 
 
     /**
